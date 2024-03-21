80af27d3fb82045638138b981884895b688d0058
Sunil Ghanchi <88071734+sunilghanchi@users.noreply.github.com>
2024-03-21 15:35:24 +0530

    app.py

--- Website Main/app.py
+++ Website Main/app.py
@@ -5,13 +5,23 @@ import pickle
 import recommendor
 import place_func
 import rest_recc
+import replicate
+import json
 import nltk
+import os
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 CORS(app)
+
+#REPLICATE_API_TOKEN="Your-Token-Here"
+
+REPLICATE_API_TOKEN = os.getenv("Token")
+
+# Global variables to store recommendations data
+recommendations_data = {'hotels': None, 'attractions': None, 'restaurants': None}
 
 # Load pickled DataFrames
 with open('hotel_dataframe.pkl', 'rb') as f:
@@ -23,23 +33,29 @@ with open('places_dataframe.pkl', 'rb') as f:
 with open('rest_dataframe.pkl', 'rb') as f:
     restaurant_df = pickle.load(f)
 
+
 @app.route('/')
 def index():
     return render_template('main.html')
 
 @app.route('/get_recommendations', methods=['POST'])
 def get_recommendations():
+    global recommendations_data  # Access the global variable
+
     # Get form data
     city = request.form['destination']
-    hotel_features = request.form['hotel_features']
-    attraction_features = request.form['attraction_features']
-    restaurant_description = request.form['restaurant_description']
-    restaurant_locality = request.form['restaurant_locality']
-    hotel_price = request.form['hotel_price']
+    days = request.form['days']
+    hotel_features = request.form['hotel-features']
+    attraction_features = request.form['attraction-features']
+    restaurant_description = request.form['restaurant-description']
+    restaurant_locality = request.form['restaurant-locality']
+    hotel_price = request.form['hotel-price']
 
-    # Process form data and get hotel recommendations
+    # Process form data and get recommendations
     hotel_recommendations = recommendor.recommendor(city, hotel_features, hotel_price, hotel_df)
-
+    attraction_recommendations = place_func.requirementbased(city, attraction_features, main_df)
+    restaurant_recommendations = rest_recc.rest_recc(city, restaurant_description, restaurant_locality, restaurant_df)
+    
     # Check if hotel recommendations are None
     if hotel_recommendations is None:
         # Return a JSON response indicating that the city is not in the database
@@ -57,13 +73,34 @@ def get_recommendations():
         # Return a JSON response indicating that restaurant recommendations are not available
         return jsonify({'error': f'No restaurants recommendations available for {city}. We will update soon. Thank you for your cooperation.'})
 
-    # Prepare recommendations data
-    hotel_data = hotel_recommendations.to_dict(orient='records')
-    attraction_data = attraction_recommendations.to_dict(orient='records')
-    restaurant_data = restaurant_recommendations.to_dict(orient='records')
+    # Update the global variable with recommendations data
+    recommendations_data['hotels'] = hotel_recommendations.to_dict(orient='records')
+    recommendations_data['attractions'] = attraction_recommendations.to_dict(orient='records') 
+    recommendations_data['restaurants'] = restaurant_recommendations.to_dict(orient='records') 
 
-    # Return recommendations as JSON response
-    return jsonify({'hotels': hotel_data, 'attractions': attraction_data, 'restaurants': restaurant_data})
+    # Prepare and return recommendations as JSON response
+    iter_data = f'City of travel: {city}, days of travel: {days}, {recommendations_data}'
+    client = replicate.Client(api_token=REPLICATE_API_TOKEN)
+    output = ""
+    if recommendations_data['hotels'] is not None:
+        for event in client.stream(
+            "mistralai/mixtral-8x7b-instruct-v0.1",
+            input={
+                "top_k": 50,
+                "top_p": 0.9,
+                "prompt": "Generate a detailed trip itinerary based on the parsed data, covering the destination in the provided number of days. Ensure that the itinerary includes changing the accommodation for every day and does not exceed the specified number of days, shorten the hotel details. Please provide the number of days for the trip explicitly." + iter_data,
+                "temperature": 0.01,
+                "max_new_tokens": 1024,
+                "prompt_template": "<s>[INST] {prompt} [/INST] ",
+                "presence_penalty": 0,
+                "frequency_penalty": 0
+            },
+        ):
+            output += str(event)
+    iter = json.dumps({"output": output})
+    
+    return jsonify(recommendations_data,iter)
+    # return (output)
 
 @app.route('/about')
 def about():
